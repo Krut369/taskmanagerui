c01bce4205cccd1132a21726536361e0fb4415a1
Krutarth Solanki <119230368+Krut369@users.noreply.github.com>
2025-08-09 16:21:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart'; // Add this import
+import 'package:device_preview/device_preview.dart'; 
 import 'screens/task_list_screen.dart';
 
 void main() {
   runApp(
     DevicePreview(
-      enabled: true, // Set to false in production
+      enabled: true, 
       builder: (context) => const MyApp(),
     ),
   );
